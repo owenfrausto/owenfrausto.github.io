9c318ba9df88184d4ccf7e96b4d9c89b07cd6df6
Owen Frausto <owenf@seas.upenn.edu>
2023-07-18 23:51:03 -0700

    Added working slider

--- energy-projects/solarscript.js
+++ energy-projects/solarscript.js
@@ -5,6 +5,7 @@
  * 	Handles control and loading of my solar project
  */
 
+/*
 class SliderInput {
 	constructor(div){
 		console.log(typeof div);
@@ -12,12 +13,33 @@ class SliderInput {
 		const number = div.children("#slider-input-number")[0];
 	}
 }
+*/
+
+// Setup slider-input update handlers
+function bound(lower, upper, value){
+	if(value <= upper && value >= lower){
+		return value;
+	} else if(value < lower){
+		return lower;
+	} else{
+		return upper;
+	}
+}
 
 $(document).ready(function(){
 	console.log("solarscript.js running on document ready");
+	
+	$(".slider-input").on("change", event =>{
+		let slider = event.currentTarget;
+		let v = bound(-90, 90, event.target.value);
+		slider.querySelectorAll(".slider-input-number")[0].value = v;
+		slider.querySelectorAll(".slider-input-range")[0].value = v;
+	});
+
+
 
 	//Initialize all slider inputs with class functions
-	$(".slider-input").each(i => {
-		new SliderInput( $(".slider-input")[i] );
-	});
+	//$(".slider-input").each(i => {
+	//	new SliderInput( $(".slider-input")[i] );
+	//});
 });
